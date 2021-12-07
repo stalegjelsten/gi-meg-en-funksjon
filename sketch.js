ac7bc9e27b7c4fcfb38fe1e41703316c00dc0cf2
Ståle Gjelsten <stalegjelsten@gmail.com>
2021-12-07 16:20:12 +0100

    added shake

--- sketch.js
+++ sketch.js
@@ -9,70 +9,80 @@ let funksjoner = [{
     "matNavn": "Identitetsfunksjonen",
     "formel": "y=x",
     "forkl": "Gi tilbake det samme tallet.",
-    "url": "https://www.google.com/search?y=x"
+    "url": "https://www.google.com/search?y=x",
+    "eqn": "x"
 },
 {
     "navn": "Positivitets&shy;funksjonen",
     "matNavn": "Absoluttverdifunksjonen",
     "formel": "y = \\vert x \\vert",
     "forkl": "Hvis tallet er positivt gir du tallet tilbake. Hvis tallet er negativt så tar du bort minustegnet og gir tallet tilbake.",
-    "url": "http://google.com/search?y=abs(x)"
+    "url": "http://google.com/search?y=abs(x)",
+    "eqn": "Math.abs(x)"
 },
 {
     "navn": "Har du dobla dosen eller?",
     "matNavn": "",
     "formel": "y = 2x",
     "forkl": "Doble tallet du får og gi det tilbake.",
-    "url": "http://google.com/search?y=2x"
+    "url": "http://google.com/search?y=2x",
+    "eqn": "2*x"
 },
 {
     "navn": "Negativitets&shy;funksjonen",
     "matNavn": "Den negative absoluttverdifunksjonen",
     "formel": "y = - \\vert x \\vert",
     "forkl": "Hvis tallet er negativt gir du tallet tilbake. Hvis tallet er positivt så legger du til et minustegn og gir tallet tilbake.",
-    "url": "http://google.com/search?y=-abs(x)"
+    "url": "http://google.com/search?y=-abs(x)",
+    "eqn": "-Math.abs(x)"
 },
 {
     "navn": "Gidder-ikke-i-dag-funksjonen",
     "matNavn": "Nullfunksjonen",
     "formel": "y = 0",
     "forkl": "Gi tilbake tallet null. Uansett hvilket tall du får..",
-    "url": "http://google.com/search?y=0"
+    "url": "http://google.com/search?y=0",
+    "eqn": "0"
 },
 {
     "navn": "Ca 2",
     "matNavn": "Konstant funksjon. 2.",
     "formel": "y = 2",
     "forkl": "Gi tilbake tallet 2. Uansett hvilket tall du får.",
-    "url": "http://google.com/search?y=2"
+    "url": "http://google.com/search?y=2",
+    "eqn": "20"
 },
 {
     "navn": "Jeg vokser fort",
     "matNavn": "Andregradsfunksjon",
     "formel": "y = x^2",
     "forkl": "Gang tallet du får med seg selv og gi det tilbake.",
-    "url": "http://google.com/search?y=x^2"
+    "url": "http://google.com/search?y=x^2",
+    "eqn": "x**2 / 100"
 },
 {
     "navn": "Rett linje som stiger",
     "matNavn": "Lineær funksjon",
     "formel": "y = 3x - 1",
     "forkl": "Multipliser tallet du får med 3 og trekk deretter fra 1.",
-    "url": "http://google.com/search?y=3x-1"
+    "url": "http://google.com/search?y=3x-1",
+    "eqn": "3*x-10"
 },
 {
     "navn": "Rett linje som synker",
     "matNavn": "Lineær funksjon",
     "formel": "y = -2x + 2",
     "forkl": "Multipliser tallet du får med -2 (minus 2) og legg deretter til 2.",
-    "url": "http://google.com/search?y=-2+2"
+    "url": "http://google.com/search?y=-2+2",
+    "eqn": "-2*x+20"
 },
 {
     "navn": "Jeg vokser veeeeeldig fort",
     "matNavn": "Tredjegradsfunksjon",
     "formel": "y = x^3",
     "forkl": "Opphøy tallet i 3 og gi det tilbake. Husk at tallene beholder fortegnet sitt når de opphøyes i 3 (og andre oddetall).",
-    "url": "http://google.com/search?y=x^3"
+    "url": "http://google.com/search?y=x^3",
+    "eqn": "(x/20)**3"
 }
 ]
 
@@ -124,11 +134,46 @@ function draw() {
 
     if (pressCount % 24 == 0 && pressCount != 0) {
         textSize(300)
-        text("🎅", width/2-150, height/2+150)
+        text("🎅", width/2-150, height-50)
     }
+
+
+    // translate(width/2, height-200)
 
 }
 
+function drawGraph(eq) {
+    push()
+    strokeWeight(2)
+    line(width/2, height, width/2, height-400)
+    line(width/2-200, height-200, width/2+200, height-200)
+    pop()
+    
+    push()
+    stroke("hsb(200, 80%, 80%)")
+    strokeWeight(2)
+    noFill()
+    translate(width/2, height-200)
+    beginShape()
+    for (let x = -100; x < 100; x++) {
+        vertex(x,-eval(eq))
+    }
+    endShape()
+    pop() 
+
+}
+
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        drawGraph(funksjon.eqn)
+    } else if (keyCode == RIGHT_ARROW) {
+        mousePressed()
+    }
+}
+
+function deviceShaken() {
+    drawGraph(funksjon.eqn)
+}
 
 function mousePressed() {
     while (funcNo == oldFuncNo) {
